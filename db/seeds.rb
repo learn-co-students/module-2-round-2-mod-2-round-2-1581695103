5189ffdd02f675c38fe5dd5bbec2572ad9e0c0d1
Paul Nicholsen <paulnicholsen27@gmail.com>
2020-01-13 17:08:00 -0500

    Update seeds.rb
    typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Power.destroy_all
 powers = [
   {name: "super strength", description: "gives the wielder super-human strengths" },
   {name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed" },
-  {name: "super human senses", description: "allows the wielder to user her senses at a super-human level" },
+  {name: "super human senses", description: "allows the wielder to use her senses at a super-human level" },
   {name: "elasticity", description: "can stretch the human body to extreme lengths" }
 ]
 
