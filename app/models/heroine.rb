9b59defa6bc578ec9b07e2ecd53b17e68e7420d3
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-02-14 11:17:01 -0600

    maxsubmit

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,10 @@
 class Heroine < ApplicationRecord
+    has_many :heroine_powers
+    has_many :powers, through: :heroine_powers
+
+    validates :name, presence: true
+    validates :super_name, presence: true 
+    validates :super_name, uniqueness: true
+
+    accepts_nested_attributes_for :powers
 end
